c502fe7b82446e145c5826f84846ff3f48bae904
devakshayy <devakshay916@gmail.com>
2024-12-26 12:04:01 +0530

    the quote page completed

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import videoTumbnail from '/video-thumbnail.png'
+import { SiTicktick } from "react-icons/si";
+
 
 const Quotes = () => {
+
+  const tick = (
+      <>
+        <SiTicktick className="text-[14px] text-[#408DFB]" />
+      </>
+    );
+    
   let sectionStyle = {
       width: "400px",
       height: "200px",
@@ -35,10 +44,36 @@ const Quotes = () => {
          {/* Zoho Video Play button end */}
      </div>
 
-              {/* second div */}
-     <div>
-        {/* second div */}
+     {/* The Quotes workflw start */}
+     <div className='flex flex-col gap-8 items-center bg-[#f9f9fb] pt-[20px] pb-[45px]'>
+          <div>Life cycle of Quote</div>
+          <div> 
+            <img src="/quotes-workflow.png" alt="quotesworkflow" />
+          </div>
+          <div className="w-[800px] border-t-2 flex h-[260px]">
+          <div className="w-1/4 h-full "></div>
+          <div className="pt-[50px]">
+            <div className="text-xs pb-3">In the Quotes module, you can:</div>
+            <ul className="flex flex-col gap-1">
+              <li className="flex gap-2 text-[16px] items-center">
+                {" "}
+                {tick}
+                Customize your quote {" "}<span className='cursor-pointer text-blue-500'>learn More</span>
+              </li>
+              <li className="flex gap-2 text-[16px] items-center">
+                {tick}
+                Convert an accepted quote into an invoice
+              </li>
+              <li className="flex gap-2 text-[16px] items-center">
+                {tick}
+               Know when an quote has been viewed,accepted,or declined{" "}
+                <span className='cursor-pointer text-blue-500' > Learn More</span>
+              </li>
+            </ul>
+          </div>
+          </div>
      </div>
+     {/* The Quotes workflw start */}
     </>
    
   )
