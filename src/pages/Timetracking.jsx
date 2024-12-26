e8fa1423cfb3930a1737ae30cbc836912aa386f4
devakshayy <devakshay916@gmail.com>
2024-12-26 19:29:34 +0530

    the time tracking page completed

--- src/pages/Timetracking.jsx
+++ src/pages/Timetracking.jsx
@@ -1,5 +1,10 @@
 import videoTumbnail from '/video-thumbnail.png'
 import { SiTicktick } from "react-icons/si";
+import { FaApple } from "react-icons/fa";
+import { DiAndroid } from "react-icons/di";
+import { FaWindows } from "react-icons/fa";
+import { BsSmartwatch } from "react-icons/bs";
+
 
 const Timetracking = () => {
    const tick = (
@@ -41,17 +46,20 @@ const Timetracking = () => {
        </div>
   
        {/* The Quotes workflw start */}
-       <div className='flex flex-col gap-8 items-center bg-[#f9f9fb] pt-[30px] pb-[45px]'>
+       <div className='flex flex-col  gap-8 items-center bg-[#f9f9fb] pt-[30px] pb-[45px]'>
             <div className='text-xs'>Life cycle of a Project</div>
             <div> 
               <img src="/projectsworkflow.png" alt="projectsworkflow" />
             </div>
-            <div className='flex flex-col items-center gap-4 border-t-[1px]'>
-            <div className='text-xs'>Do more than just Project Time Tracking</div>
-                  <div className='flex'>
-                        {/* project ul div Start */}
-                 <div>
-                  <ul className="flex flex-col gap-1">
+            <div className='flex border-t-[1px]  flex-col gap-4 px-20 py-10'>
+              <div className='flex justify-center'>
+                <div className='text-xs'>Do more than just Project Time Tracking</div>
+              </div>
+                <div className='flex gap-28'>
+                       
+ {/* project ul div Start */}
+<div className="the-ul-container">
+<ul className="flex flex-col p-2 w-[400px] justify-center gap-1">
                     <li className="flex gap-2 text-[16px] items-center">
                       {" "}
                       {tick}
@@ -68,22 +76,33 @@ const Timetracking = () => {
                     <li className="flex gap-2 text-[16px] items-center">
                       {tick}
                       Manage projects and associated project tasks{" "}
-                      <span className='text-[#408DFB]'>Learn More</span>
                     </li>
+                    <li className='text-[16px]' ><span className='cursor-pointer text-[#408DFB]'>Learn More</span> </li>
                   </ul>
-                 </div>
-                 {/* project ul div End */}
-                 {/* Track Time and Projects Effortlessly div Start */}
-                  <div>
-                    <div className="text-xs pb-3">In the Expenses module, you can:</div>
+</div>
+  {/* project ul div End */}
+  
+{/* Track Time and Projects Effortlessly div Start */}
+  <div className='flex  p-4   gap-2 flex-col'>
+                    <div className="text-[16px]">Track Time and Projects Effortlessly</div>
                     <div className="text-[14px]" >
                     Manage your invoices, quotes, track payments
                     and upload expense receipts right from your
                     phone. Or maybe your tablet!
                     </div>
+                    <div className='flex items-center text-lg cursor-pointer text-[#6C718A] gap-4'>
+                      <BsSmartwatch />
+                      <FaApple />
+                      <DiAndroid />
+                      <FaWindows />
+                    </div>
+                    <div className='flex  items-center gap-1 text-[#408dfb] text-[13px] border-t-[1px] py-2'>
+                        <img className='w-3 h-3' src="/chromelogo.png" alt="chromelogo" />
+                         <span className='cursor-pointer'>Try out our Google Chrome Extension!</span>
+                    </div>
                   </div>
-                 {/* Track Time and Projects Effortlessly div End */}
-                  </div>
+{/* Track Time and Projects Effortlessly div End */}
+                </div>
             </div>
        </div>
        {/* The Quotes workflw start */}
